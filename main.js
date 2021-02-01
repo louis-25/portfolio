a46cbd8438713e5ec3c59364091f331032d71651
louis-25 <20151577@vision.hoseo.edu>
2021-02-01 15:27:02 +0900

    JS:Navbar menu click

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) { //링크가 있는경우만 실행
+        return;
+    }
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth'});
+    
+
+});
